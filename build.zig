20b573cf3bdf9aadbcf5e7fb157dcb83a8e1809f
Matt Porter <map@qompass.ai>
2025-04-09 11:13:42 -0700

    Create build.zig

---
+++ build.zig
@@ -0,0 +1,7 @@
+try zine.website(b, .{
+    .title = "The Deep Pursuit of Quality AI",
+    .host_url = "https://phaedrusflow.github.io",
+    .layouts_dir_path = "layouts",
+    .content_dir_path = "content",
+    .assets_dir_path = "assets",
+});
