1bdcb02d56437ba00c39822e76a8d547318ead2a
phaedrusflow <map@qompass.ai>
2025-07-26 12:35:35 -0700

    update

--- d.sh
+++
@@ -1,5 +0,0 @@
-# d.sh
-# Qompass AI - [Add description here]
-## Copyright (C) 2025 Qompass AI, All rights reserved
-# ---------------------------------------- Copyright (C) 2025 Qompass AI, All rights reserved
-# ----------------------------------------
